e87eb382518211c7dc72b8afbbaf118348952396
phil0522 <phonica@gmail.com>
2023-10-02 07:58:21 -0700

    Adds a sample component.

---
+++ web/src/app/components/components.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyButtonComponent } from './my-button/my-button.component';
+
+
+
+@NgModule({
+  declarations: [
+    MyButtonComponent
+  ],
+  exports: [
+    MyButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
---
+++ web/src/app/components/my-button/my-button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-button',
+  templateUrl: './my-button.component.html',
+  styleUrls: ['./my-button.component.scss']
+})
+export class MyButtonComponent {
+  title: string
+  public constructor() {
+    this.title = "unname"
+  }
+}
